294a30236ce0d8d2b40dba1f0248714576a36c78
echodavid <despinosa2550@gmail.com>
2025-03-17 17:07:41 -0600

    init rest-api

---
+++ database/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'user_api',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.TEST_DB_NAME || 'test_database',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.PROD_DB_NAME || 'production_database',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  }
+};
