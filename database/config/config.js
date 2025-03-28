f271f14bf586bed16aef31e4897626a1b53366b1
echodavid <despinosa2550@gmail.com>
2025-03-28 10:33:49 -0600

    adding ssl option

--- database/config/config.js
+++ database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -6,20 +6,29 @@ module.exports = {
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.DB_NAME || 'user_api',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: process.env.DB_SSL === 'true' ? { require: true } : false
+    }
   },
   test: {
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.TEST_DB_NAME || 'test_database',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: process.env.DB_SSL === 'true' ? { require: true } : false
+    }
   },
   production: {
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || 'password',
     database: process.env.PROD_DB_NAME || 'production_database',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: process.env.DB_SSL === 'true' ? { require: true } : false
+    }
   }
 };
