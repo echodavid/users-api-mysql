f73b95d61c5a970336225afe80f2deece2e0e60d
echodavid <despinosa2550@gmail.com>
2025-04-25 10:36:42 -0600

    adding edit and update

--- database/config/config.js
+++ database/config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL', // Corregido: use_env_variable
+    use_env_variable: 'DATABASE_URL', 
     dialect: 'mysql',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // Permitir conexiones SSL sin verificación estricta
+        rejectUnauthorized: false, 
       },
     },
   },
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome Mysql API!'));
 
+router.get('/users/:id', controllers.getUserById);
+router.put('/users/:id', controllers.updateUser);
 router.post('/users', controllers.createUser);
 router.get('/users', controllers.getAllUsers);
+router.delete('/users/:id', controllers.deleteUser);
+
 
 module.exports = router;
